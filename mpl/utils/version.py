01193ebefbe5fb3a817ed9a43d5f82527cc1f177
Jadsonlucio <jadsonaluno@hotmail.com>
2019-02-15 13:17:47 -0300

    add utils file and version manage file

---
+++ mpl/utils/version.py
@@ -0,0 +1,16 @@
+def get_version(version = None ) -> str:
+    """ 
+    Return string with formated version.
+    
+    Parameters
+    ----------
+    version : tuple or list
+        Tuple of version infos.
+
+    Returns
+    ----------
+    str
+        Version string.
+    """
+
+    return ''.join(version)
