516d06e57952bff94bbd4337b9aa5d68b5fcff42
Jadsonlucio <jadsonaluno@hotmail.com>
2019-02-15 16:45:10 -0300

    add version info log

--- mpl/scripts/main.py
+++ mpl/scripts/main.py
@@ -1,6 +1,18 @@
 import click
+from .. import __version__
 
-@click.command()
+
+def version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
+
+@click.group()
+@click.option('--version', is_flag=True, callback=version,
+              expose_value=False, is_eager=True)
 def cli():
     """ Welcome to Model processing library """
     pass
+
+
