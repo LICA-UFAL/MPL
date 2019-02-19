5214ff788bbe2977331c5a0ab4d4503bcebf9c01
jadsonlucio <jadsonaluno@hotmail.com>
2019-02-18 23:25:25 -0300

    add project manage file

---
+++ mpl/utils/project.py
@@ -0,0 +1,10 @@
+import os
+
+SCAFFOLD_PROJECTS_PATH = "scaffolds/"
+
+def create_project():
+    pass
+
+def get_scaffolds():
+    """ Return an list of project scaffold paths """
+    return [SCAFFOLD_PROJECTS_PATH + file_name for file_name in os.listdir(SCAFFOLD_PROJECTS_PATH)]
