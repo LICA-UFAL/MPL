7e7d566f692dc9b50186bce5c8e8e65304587a0c
jadsonlucio <jadsonaluno@hotmail.com>
2019-09-12 11:10:12 -0300

    add build script running in installation process

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+from mpl import __version__
+from mpl.utils.version import get_version
+from mpl.scripts.builder import builde_mpl_cache_folder
+
+class InstallWrapper(install):
+    """
+    Provide an install wrapper that run all of builder processing.
+    """
+
+    def run(self):
+        install.run(self)
+        builde_mpl_cache_folder()
+
 
 setup(
     name='mpl',
-    version='0.0.1',
+    version=get_version(__version__),
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -12,4 +27,5 @@ setup(
         [console_scripts]
         mpl=mpl.scripts.main:cli
     ''',
+    cmdclass={'install': InstallWrapper},
 )
