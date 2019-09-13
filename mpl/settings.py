61634b3f82d018ad03d9c490e03546cc7d62132b
jadsonlucio <jadsonaluno@hotmail.com>
2019-09-13 18:56:48 -0300

    add new project settings variables

--- mpl/settings.py
+++ mpl/settings.py
@@ -1,4 +1,11 @@
 from mpl.utils.platform import config 
 
-MPL_CACHE_PATH = config.CACHE_PATH + "/mpl/"
-SCAFFOLDS_FOLDER_PATH = MPL_CACHE_PATH + "/scaffolds"
+# cache path settings
+MPL_CACHE_PATH = config.CACHE_PATH
+MPL_CONFIG_FILE_PATH = MPL_CACHE_PATH + "/mpl/" +"/config.json"
+SCAFFOLDS_FOLDER_PATH = MPL_CACHE_PATH + "/mpl/" + "/scaffolds"
+PROJECTS_FOLDER_PATH = MPL_CACHE_PATH + "/mpl/" + "/projects"
+PROJECT_CACHE_SCAFFOLD_PATH = MPL_CACHE_PATH + "/mpl/" + "/projects/project_cache_scaffold.json"
+
+# file path settings
+MPL_CACHE_SCAFFOLD_PATH = "mpl/files/mpl_cache_scaffold.json"
