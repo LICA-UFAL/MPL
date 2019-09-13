fa56a1d4f5b07a99a9acd89320f81f1ea6fcc533
jadsonlucio <jadsonaluno@hotmail.com>
2019-09-13 19:00:41 -0300

    replace choicefile class to choice element

--- mpl/utils/custom/click_types.py
+++ mpl/utils/custom/click_types.py
@@ -1,31 +1,33 @@
 import os
 import click 
 
-class Choice_file(click.Choice):
+class ChoiceElement(click.Choice):
     name = "choice"
-    def __init__(self,files_paths, description):
-        self.files_paths = files_paths + ["custom"]
+    def __init__(self, elements, description):
+        self.elements = ["None" ,"custom"] + elements
         self.description = description
-        super().__init__(self.files_names)
+        super().__init__(self.elements_with_index)
 
-    
     def convert(self, value, param, ctx):
         try:
             value_index = int(value)
-            if value_index == len(self.files_paths) - 1:
+            if value_index == 0:
+                return None
+            elif value_index == None:
                 return click.prompt(self.description, type = click.Path())
             else:
-                return self.files_paths[value_index]
+                return self.elements[value_index]
+
         except ValueError as e:
             self.fail('invalid choice: %s. (choose from %s)' %
-                  (value, ', '.join(self.files_names)), param, ctx)
+                  (value, ', '.join(self.elements_with_index)), param, ctx)
         except IndexError as e:
             self.fail('invalid choice: %s. (choose from %s to %s)' %
-                  (value, 0, str(len(self.files_names) - 1)), param, ctx)
+                  (value, 0, str(len(self.elements_with_index) - 1)), param, ctx)
 
     def __repr__(self):
-        return 'Choice(%r)' % list(self.files_names)
+        return 'Choice(%r)' % list(self.elements_with_index)
 
     @property
-    def files_names(self):
-        return [str(count) + " - " + os.path.basename(file_path) for count,file_path in enumerate(self.files_paths)]
+    def elements_with_index(self):
+        return [str(count) + " - " + str(element) for count,element in enumerate(self.elements)]
