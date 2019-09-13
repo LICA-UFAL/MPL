52ebbfe56f821df954189a758a3d9df0862646c1
jadsonlucio <jadsonaluno@hotmail.com>
2019-09-13 18:57:33 -0300

    implement builde_mpl_cache_folder function

--- mpl/scripts/builder.py
+++ mpl/scripts/builder.py
@@ -1,21 +1,11 @@
 import os
 from distutils.dir_util import copy_tree
 
-from mpl.settings import MPL_CACHE_PATH, SCAFFOLDS_FOLDER_PATH
-
-
-INSTALL_SCAFFOLDS_FILE_PATH = "mpl/scaffolds"
+from mpl.utils.scaffold.scaffold import Scaffold
+from mpl.settings import MPL_CACHE_PATH, MPL_CACHE_SCAFFOLD_PATH
 
 # post installation script
 def builde_mpl_cache_folder():
     """ Create mpl cache folder, copping scaffolds and saving config files. """
-
-    if not os.path.exists(MPL_CACHE_PATH):
-        os.makedirs(MPL_CACHE_PATH)
-
-    copy_scaffolds_files()
-
-def copy_scaffolds_files():
-    """ Copy all default scaffold config files to cache path. """
-
-    copy_tree(INSTALL_SCAFFOLDS_FILE_PATH, SCAFFOLDS_FOLDER_PATH)
+    
+    Scaffold.load_from_file(MPL_CACHE_SCAFFOLD_PATH).run(MPL_CACHE_PATH)
